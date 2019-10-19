6d9931197dc8cc57a255e15f15d70e7fd6d8fccd
wassfila <wassim.filali@gmail.com>
2019-10-19 12:14:52 +0200

    svg test example

---
+++ svg_interactive/show.js
@@ -0,0 +1,10 @@
+function showArea(event){
+    var pop = document.getElementById("trec_pop");
+    if(pop.style.visibility == "visible")    {
+        pop.style.visibility = "hidden";
+    }
+    else{
+        pop.style.visibility = "visible";
+    }
+ }
+ 
